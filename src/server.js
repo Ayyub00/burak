e4c2c8c4e4980f4e7624647e738f5e1215d2b411
Ayyub00 <Sobirovayubxon4@gmail.com>
2024-09-03 18:26:20 +0900

    feat: TASK -G

--- src/server.js
+++ src/server.js
@@ -6,3 +6,22 @@ var currentTime = (0, moment_1.default)().format("YYYY MM DD");
 console.log(currentTime);
 var person = "MARTIN";
 var count = 100;
+
+
+Task-G method 2
+function getHighestIndex(arr) {
+    let maxValue = arr[0];
+    let maxIndex = 0;
+
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] > maxValue) {
+            maxValue = arr[i];
+            maxIndex = i;
+        }
+    }
+
+    return maxIndex;
+}
+
+let index = getHighestIndex([5, 21, 12, 21, 8]);
+console.log('method_1:', index);
