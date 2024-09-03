df22c4f90b4a6ea50d52cb93289f9f97c3d13287
Ayyub00 <Sobirovayubxon4@gmail.com>
2024-09-03 16:43:58 +0900

    BRR: Initial Setup

---
+++ src/server.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+console.log("EXUCUTED");
+var moment_1 = require("moment");
+var currentTime = (0, moment_1.default)().format("YYYY MM DD");
+console.log(currentTime);
+var person = "MARTIN";
+var count = 100;
