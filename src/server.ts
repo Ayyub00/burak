cd85b35cb823b60c2a780b841e79f9f2806d5722
Ayyub00 <Sobirovayubxon4@gmail.com>
2024-09-04 19:36:24 +0900

    feat: integration of environmental variables

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,22 @@
- console.log("EXUCUTED");
+//  console.log("EXUCUTED");
 
-import moment from "moment";
+// import moment from "moment";
 
-const currentTime = moment().format("YYYY MM DD");
-console.log(currentTime);
+// const currentTime = moment().format("YYYY MM DD");
+// console.log(currentTime);
 
 
- const person: string = "MARTIN";
- const count: number = 100;
+//  const person: string = "MARTIN";
+//  const count: number = 100;
+
+// Architectural pattern: MVC, Dependency Injection, MVP
+
+// Design pattern: Middleware, Decotar
+
+
+import dotenv from "dotenv";
+dotenv.config();
+
+console.log("PORT:",process.env.PORT);
+
+console.log("MONGO_URL:", process.env.MONGO_URL);
