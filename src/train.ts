4f2ce53cf50fc48ebb21f0d68631fa10d082eef2
Ayyub00 <Sobirovayubxon4@gmail.com>
2024-09-05 13:53:23 +0900

    feat: express integrations

--- src/train.ts
+++ src/train.ts
@@ -1,1 +1,10 @@
-console.log("HELLO World!");
+// H-TASK
+function getPositive(a: number[]): string {
+  const positives = a.filter((num) => num > 0);
+  return positives.join("");
+}
+
+
+
+
+console.log(getPositive([1, -4, 2, 1]));
