0bfa3a9c1235d59d6ea3f1c3a7fa18d7c42c5a96
Ayyub00 <Sobirovayubxon4@gmail.com>
2024-09-07 15:19:21 +0900

    feat: member servise model integration

--- src/controllers/restaurant.controller.ts
+++ src/controllers/restaurant.controller.ts
@@ -1,29 +1,31 @@
+
 import { Request, Response } from "express";
- import { T } from "../libs/types/common";
+import { T } from "../libs/types/common";
+import { } from "../models/Member.service";
 
- const memberController: T = {};
- memberController.goHome = (req: Request, res: Response) => {
-   try {
-     res.send("Home Page");
-   } catch (err) {
-     console.log("Error, goHome:", err);
-   }
- };
+const restaurantController: T = {};
+restaurantController.goHome = (req: Request, res: Response) => {
+  try {
+    res.send("Home Page");
+  } catch (err) {
+    console.log("Error, goHome:", err);
+  }
+};
 
- memberController.getLogin = (req: Request, res: Response) => {
-   try {
-     res.send("Login Page");
-   } catch (err) {
-     console.log("Error, getLogin:", err);
-   }
- };
+restaurantController.getLogin = (req: Request, res: Response) => {
+  try {
+    res.send("Login Page");
+  } catch (err) {
+    console.log("Error, getLogin:", err);
+  }
+};
 
- memberController.getSignup = (req: Request, res: Response) => {
-   try {
-     res.send("Signup Page");
-   } catch (err) {
-     console.log("Error, getSignup:", err);
-   }
- };
+restaurantController.getSignup = (req: Request, res: Response) => {
+  try {
+    res.send("Signup Page");
+  } catch (err) {
+    console.log("Error, getSignup:", err);
+  }
+};
 
- export default memberController;
+export default restaurantController;
