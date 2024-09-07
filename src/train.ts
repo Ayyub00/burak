28c85e3d06d2a2066376481f7ccad5d53e82e8ff
Ayyub00 <Sobirovayubxon4@gmail.com>
2024-09-07 15:44:33 +0900

    feat: TASK H2

--- src/train.ts
+++ src/train.ts
@@ -1,3 +1,14 @@
+// H2-TASK
+function getDigits(a: string): string {
+  let numbers: string[] = [];
+  for (let i = 0; i < a.length; i++) {
+    if (!isNaN(parseInt(a[i]))) {
+      numbers.push(a[i]);
+    }
+  }
+  return numbers.join("");
+}
+
 // H-TASK
 function getPositive(a: number[]): string {
   const positives = a.filter((num) => num > 0);
