fe25a4d48c37bf4f7125f714b7f6ac883579713a
Ayyub00 <Sobirovayubxon4@gmail.com>
2024-09-07 13:58:11 +0900

    feat: create routing and member controller

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express from "express";
  import path from "path";
+ import router from "./router";
 
  /** 1 - ENTRENCE **/
  const app = express();
@@ -14,5 +15,6 @@ import express from "express";
  app.set("view", path.join(__dirname, "views"));
  app.set("view engine", "ejs");
  /** 4 - ROUTERS **/
+ app.use("/", router);
 
  export default app; // module.exports
