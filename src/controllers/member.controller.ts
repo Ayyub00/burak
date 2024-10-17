c2f33d0ba5c32710697509746d0064301f186f1c
Ayyub00 <Sobirovayubxon4@gmail.com>
2024-10-17 20:30:31 +0900

    feat: added logout method and modify getMemberDetail business logic

--- src/controllers/member.controller.ts
+++ src/controllers/member.controller.ts
@@ -101,7 +101,7 @@ memberController.verifyAuth = async (
   } catch (err) {
     console.log("Error, verifyAuth:", err);
     if (err instanceof Errors) res.status(err.code).json(err);
-    else res.status(Errors.standard.code).json(Errors.standard);
+    else res.status(Errors.standart.code).json(Errors.standart);
   }
 };
 memberController.retrieveAuth = async (
