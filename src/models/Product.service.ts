d49096f1ba47c20124749823ef9ae26af0469d42
Ayyub00 <Sobirovayubxon4@gmail.com>
2024-10-26 04:34:45 +0900

    feat: modify getProduct business logic

--- src/models/Product.service.ts
+++ src/models/Product.service.ts
@@ -7,11 +7,18 @@ import { ProductStatus } from "../libs/enums/product.enum";
 import { T } from "../libs/types/common";
 import { ObjectId } from "mongoose";
 import restaurantController from "../controllers/restaurant.controller";
+import ViewService from "./View.service";
+import { ViewInput } from "../libs/types/view";
+import { ViewGroup } from "../libs/enums/view.enum";
+
 
 class ProductSevice {
     private readonly productModel;
+    public viewService;
+
   constructor() {
     this.productModel = ProductModel;
+    this.viewService = new ViewService();
   }
    /** SPA */
 
@@ -49,14 +56,43 @@ class ProductSevice {
   ): Promise<Product> {
     const productId = shapeIntoMongooseObjectId(id);
 
-    let result = await this.productModel.findOne({
-      _id: productId,
-      productStatus: ProductStatus.PROCESS,
-    }).exec();
-  if(!result) throw new Errors(HttpCode.NOT_FOUND, Message.NO_DATA_FOUND);
+    let result = await this.productModel
+      .findOne({
+        _id: productId,
+        productStatus: ProductStatus.PROCESS,
+      })
+      .exec();
+    if (!result) throw new Errors(HttpCode.NOT_FOUND, Message.NO_DATA_FOUND);
 
 
   // TODO: If authenticated users => first => view creation
+
+  if (memberId) {
+    // Check Existence
+    const input: ViewInput = {
+      memberId: memberId,
+      viewRefId: productId,
+      viewGroup: ViewGroup.PRODUCT,
+    };
+    const existView = await this.viewService.checkViewExistence(input);
+
+
+    console.log("exist:", existView);
+    if (!existView) {
+      // Insert View
+      console.log("PLANNING TO INSERT NEW VIEW");
+      await this.viewService.insertMemberView(input);
+
+      // Increase Counts
+      result = await this.productModel
+        .findByIdAndUpdate(
+          productId,
+          { $inc: { productViews: + 1 } },
+          { new: true }
+        )
+        .exec();
+    }
+  }
 
   return result;
   }
